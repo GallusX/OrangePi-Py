71b8c881461f5bf1c96ae3229180f2cc3cbc9ccb
GallusX <109997469+GallusX@users.noreply.github.com>
2023-01-07 08:53:46 +0300

    Delete opigpiogalluss.py

--- opigpiogalluss.py
+++
@@ -1,8 +0,0 @@
-import os
-def gpout(text):
-    return os.system("gpio -1 mode " + str(text) + " out")
-def gpup(text):
-    return os.system("gpio -1 write " + str(text) + " 1")
-def gpdown(text):
-    return os.system("gpio -1 write " + str(text) + " 0")
-
