891b783b4bad92858451d892c0b460eb1bdd2256
GallusX <109997469+GallusX@users.noreply.github.com>
2023-01-22 12:15:07 +0300

    Update helloworld.py

--- examples/helloworld.py
+++ examples/helloworld.py
@@ -5,6 +5,8 @@ from opigallus.draw import canvas        #
 from PIL import ImageFont, ImageDraw     #
 #Импорт библиотек-------------------------
 disp = ssd1306(port=0, address=0x3C, width=128, height=64)#инициализируем дисплей
+#ssd1306(port=Это номер порта где вам выдает адресс экрана i2cdetect -y 0 или i2cdetect -y 1, address=Это адресс дисплея который выдается в i2cdetect,
+#width=,height=) - это размеры дисплея всегда оставляйте 128х64 даже есть у вас 128х32 всё будет работать нормально
 with canvas(disp) as draw:#Создаем холст на дисплее//Дальше работаем с pillow 
     font = ImageFont.load_default()#Загружаем стандартные настройки шрифта
     font = ImageFont.truetype('/oled', size=23)#Загружаем шрифт//скидываем в папку /usr/share/fonts если такой папки нет создаем её
