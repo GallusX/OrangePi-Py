d24e8d36c071e77c10f9da1b4139256e1fa5a751
GallusX <109997469+GallusX@users.noreply.github.com>
2023-02-01 19:46:19 +0300

    Update ImageOled.py

--- examples/ImageOled.py
+++ examples/ImageOled.py
@@ -2,8 +2,6 @@ from opigallus import*
 from opigallus.display import ssd1306
 from opigallus.draw import canvas
 from PIL import ImageFont, ImageDraw, Image
-import time
-import socket
 
 disp = ssd1306(port=0, address=0x3C, width=128, height=64)  # rev.1 users set port=0
 with canvas(disp) as draw:
